4e4b8d5e77ed2cd88112a5acb84fb9c22b503f3b
Bharat Dua <bharatdua13@gmail.com>
2024-06-27 16:48:50 +0530

    date counter challenge complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [steps, setSteps] = useState(0);
+  const [count, setCount] = useState(0);
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const currentDate = new Date();
+  // console.log(currentDate);
+  const updatedDate = new Date(currentDate);
+  updatedDate.setDate(updatedDate.getDate() + count);
+  // console.log(updatedDate.getDate());
+  // let date = new Date().toLocaleDateString();
+  // let day = new Date().getDay();
+  // const date = new Date("june 21 2023");
+  // date.setDate(date.getDate() + count);
+  function stepDecrease() {
+    setSteps((step) => step - 1);
+  }
+
+  function stepIncrease() {
+    setSteps((step) => step + 1);
+  }
+  function countIncrease() {
+    setCount((c) => c + steps);
+  }
+  function countDecrease() {
+    setCount((c) => c - steps);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <button onClick={stepDecrease}>-</button>
+      <span>step : {steps}</span>
+      <button onClick={stepIncrease}>+</button>
+      <br />
+      <button onClick={countDecrease}>-</button>
+      <span>count : {count}</span>
+      <button onClick={countIncrease}>+</button>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        <span> {updatedDate.toDateString()}</span>
+      </p>
     </div>
   );
 }
